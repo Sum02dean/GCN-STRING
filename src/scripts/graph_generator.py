c1c6e61f233a023ecffa5048c2fa267af2e7963c
sum02dean <sum02dean@gmail.com>
2022-03-03 13:25:55 +0100

    Removed duplicate code

--- src/scripts/graph_generator.py
+++ src/scripts/graph_generator.py
@@ -18,26 +18,15 @@ paired_msa_path = ('/mnt/mnemo6/tao/PPI_Coevolution/CoEvo_data_STRING11.5'
                    '/511145_EggNOGmaxLevel1224_eggNOGfilteredData/pair_MSA_Nf90_PasteAlign')
 
 configs_dir = 'configs_11.5'
+
 coevo_path = ('/mnt/mnemo6/tao/PPI_Coevolution/CoEvo_data_STRING11.5'
               '/511145_EggNOGmaxLevel1224_eggNOGfilteredData/coevolutoin_result_DCA/')
-
-# ALPHA-Fold Paths
-ecoli_ext_string_to_af = "/mnt/mnemo6/damian/STRING_derived_v11.5/alphafold/mapping/83333.511145.tsv"
-string_to_pdb = '/mnt/mnemo6/damian/STRING_derived_v11.5/pdb/pdb2string.blastp.best_score.tsv'
-pdb_files_for_PDB = '/mnt/mnemo6/damian/STRING_freeze_v11.5/pdb/data/biounit/coordinates/divided/'
-
-# Tao STRING 11.5 paths
-anndata_path = '/mnt/mnemo6/tao/PPI_Coevolution/CoEvo_data_STRING11.5/511145_EggNOGmaxLevel1224_eggNOGfilteredData/STRINPhyPPI_Benchmark/allPPI_allInfo_frame.csv'
-paired_msa_path = '/mnt/mnemo6/tao/PPI_Coevolution/CoEvo_data_STRING11.5/511145_EggNOGmaxLevel1224_eggNOGfilteredData/pair_MSA_Nf90_PasteAlign'
-configs_dir = 'configs_11.5'
-coevo_path = '/mnt/mnemo6/tao/PPI_Coevolution/CoEvo_data_STRING11.5/511145_EggNOGmaxLevel1224_eggNOGfilteredData/coevolutoin_result_DCA/'
 
 # Netsurf paths
 nsp_msa = "/mnt/mnemo6/tao/PPI_Coevolution/STRING_data_11.5/511145_netsurfp2_output/_EggNOGmaxLevel1224_map2MSA"
 nsp_seq = "/mnt/mnemo6/tao/PPI_Coevolution/STRING_data_11.5/511145_netsurfp2_output/"
 
 
-df = pd.read_csv(anndata_path, sep='\t')
 # n_samples: use -1 for all
 n_samples = 10
 gm = GraphMaker(anndata_path=anndata_path)
