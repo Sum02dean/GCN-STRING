7d7b988055f4a3764cc633fa933c4ea2aa533802
sum02dean <sum02dean@gmail.com>
2022-03-10 15:50:57 +0100

    resolved #3

--- src/scripts/di_graph_net.py
+++ src/scripts/di_graph_net.py
@@ -16,14 +16,15 @@ from torch_geometric.nn import global_mean_pool, global_max_pool
 
 class GCN(torch.nn.Module):
     def __init__(self, hidden_channels=64):
-        """This class takes in 2 inputs. Each input is a graph representing a single protein structure in a protein pair.
-           Each graph is processed to seperate nerual networks, the outputs of which are then concatenated into a single
-           hiddden state. Finally this hidden state is passed through a simple dense layer for feature extraction, and then 
-           returned for a BCEWithLogitLoss (recommended for binary classificatio) method inside the main training loop.
-
+        """
+        This class takes in 2 inputs. Each input is a graph representing a single protein structure in a protein pair.
+        Each graph is processed by seperate nerual networks, the outputs of which are then concatenated into a single
+        hiddden state. Finally this hidden state is passed through a simple dense layer for feature extraction, and then 
+        returned for use with a BCEWithLogitLoss (recommended for binary classificatio) method inside the main training loop.
 
         :param hidden_channels: the number of hidden neurons to use, defaults to 64
         :type hidden_channels: int, optional
+
         """
         super(GCN, self).__init__()
 
@@ -34,10 +35,10 @@ class GCN(torch.nn.Module):
 
         #### Graph 1 ####
         # Layers (consider using class SAGEConv instead)
-        self.g1_conv1 = GCNConv(self.num_node_features, self.hidden_channels)
+        self.g1_conv_1 = GCNConv(self.num_node_features, self.hidden_channels)
         self.g1_linear_1 = Linear(self.hidden_channels, self.hidden_channels)
 
-        # Paramteric RelU
+        # Paramteric RelU (prelu)
         self.g1_prelu_1 = torch.nn.PReLU()
         self.g1_prelu_2 = torch.nn.PReLU()
 
@@ -52,10 +53,10 @@ class GCN(torch.nn.Module):
 
         #### Graph 2 ####
         # Layers (consider using class SAGEConv instead)
-        self.g2_conv1 = GCNConv(self.num_node_features, self.hidden_channels)
+        self.g2_conv_1 = GCNConv(self.num_node_features, self.hidden_channels)
         self.g2_linear_1 = Linear(self.hidden_channels, self.hidden_channels)
 
-        # Paramteric RelU
+        # Paramteric RelU (prelu)
         self.g2_prelu_1 = torch.nn.PReLU()
         self.g2_prelu_2 = torch.nn.PReLU()
 
@@ -71,7 +72,8 @@ class GCN(torch.nn.Module):
         #### Concatenated graphs ####
 
         # Layers
-        self.cat_linear = Linear(self.hidden_channels, self.hidden_channels)
+        self.cat_linear = Linear(
+            self.hidden_channels * 2, self.hidden_channels)
 
         # Paramteric RelU
         self.cat_prelu = torch.nn.PReLU()
@@ -85,8 +87,8 @@ class GCN(torch.nn.Module):
 
         #### Graph 1 ####
         # Conv block graph 1
-        x_1 = self.g1_conv1(x_1, edge_index_1)
-        x_1 = self.g1_batch_norm_2(x_1)
+        x_1 = self.g1_conv_1(x_1, edge_index_1)
+        x_1 = self.g1_batch_norm_1(x_1)
         x_1 = self.g1_prelu_1(x_1)
         x_1 = global_max_pool(x_1, batch_1)
 
@@ -98,8 +100,8 @@ class GCN(torch.nn.Module):
 
         #### Graph 2 ####
         # Conv block graph 2
-        x_2 = self.g2_conv1(x_2, edge_index_2)
-        x_2 = self.g2_batch_norm_2(x_2)
+        x_2 = self.g2_conv_1(x_2, edge_index_2)
+        x_2 = self.g2_batch_norm_1(x_2)
         x_2 = self.g2_prelu_1(x_2)
         x_2 = global_max_pool(x_2, batch_2)
 
@@ -113,5 +115,4 @@ class GCN(torch.nn.Module):
         x = torch.concat((x_1, x_2), dim=0)
         x = self.cat_batch_norm(x)
         x = self.cat_linear(x)
-
         return x
